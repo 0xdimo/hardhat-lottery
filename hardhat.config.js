690234fcc2892edff536cedb61dc9f38ead08954
Daudit <dimo.dev@outlook.com>
2023-06-11 17:57:56 +0300

    feat(config): Add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,19 @@ require("dotenv")
 
 module.exports = {
     solidity: "0.8.18",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            url: "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY",
+            accounts: "0xPrivateKey",
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+    },
     namedAccounts: {
         deployer: {
             default: 0,
