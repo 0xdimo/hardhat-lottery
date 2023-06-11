2ce5303ecb60603b98804c5138c778d35c9ee64c
Daudit <dimo.dev@outlook.com>
2023-06-11 18:09:41 +0300

    refactor(raffle): Add constants for env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv")
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
     solidity: "0.8.18",
     defaultNetwork: "hardhat",
