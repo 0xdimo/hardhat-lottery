f1ab3606b5b8ea58945d426c5ef3093d91abfba8
Daudit <dimo.dev@outlook.com>
2023-06-11 17:48:10 +0300

    chore(raffle): Add some more getters

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -136,4 +136,13 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     function getNumWords() public pure returns (uint256) {
         return NUM_WORDS;
     }
+
+    function getLatestTimestamp() public view returns (uint256) {
+        return s_lastTimestamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
+    }
+
 }
