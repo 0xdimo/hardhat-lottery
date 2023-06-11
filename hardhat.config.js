c428c1cbeee6afcf0b3f5b6151be1df9d965a46e
Daudit <dimo.dev@outlook.com>
2023-06-11 17:54:12 +0300

    feat(deployment): add namedAccounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("dotenv")
 
 module.exports = {
     solidity: "0.8.18",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
