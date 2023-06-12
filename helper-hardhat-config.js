58ba5d13f83d5991bf88b37e53f0ee38837b4d7a
Daudit <dimo.dev@outlook.com>
2023-06-12 22:07:06 +0300

    fix(deploy): Add more variables to helper-config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,7 +3,7 @@ const networkConfig = {
         name: "sepolia",
         subscriptionId: "2732",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
-        keepersUpdateInterval: "30",
+        interval: "30", //30 sec
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
@@ -11,6 +11,8 @@ const networkConfig = {
     31337: {
         name: "hardhat",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000",
+        interval: "30",
     },
 }
 
